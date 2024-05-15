e0d3d55dfefbbf65e0bfe65b72ac6782ae8a1f19
Sanjay Kushwah <sanjay.kushwah@galaxyweblinks.in>
2024-05-15 15:27:15 +0530

    update port

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ let app = express();
 let fs = require('fs');
 let https = require('https');
 
-let server = https.createServer({
+let server = https.createServer(
+  {
     key: fs.readFileSync(path.join(__dirname, '/ssl/key.pem'), 'utf8'),
     cert: fs.readFileSync(path.join(__dirname, '/ssl/server.crt'), 'utf8'),
     requestCert: true,
-    rejectUnauthorized: false
-},app);
+    rejectUnauthorized: false,
+  },
+  app
+);
 //let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let stream = require('./ws/stream');
@@ -26,4 +29,4 @@ app.get('/', (req, res) => {
 
 io.of('/stream').on('connection', stream);
 
-server.listen(8001);
+server.listen(3000);
